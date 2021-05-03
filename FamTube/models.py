3c1580fbf9d7d013c2400c35b198c7870b2243b7
AkashTike <akash.t@ahduni.edu.in>
2021-05-03 11:59:44 +0530

    Added Video Class

--- FamTube/models.py
+++ FamTube/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Video(models.Model):
+
+    published_at = models.DateTimeField()
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    channel_title = models.CharField(max_length=60)
+    thumbnail_url = models.URLField()
+    stored_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
